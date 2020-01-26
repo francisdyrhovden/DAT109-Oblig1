55bc4b26d2199098117120e69ede5bb97139ad69
Francis Soliman <soliman_61@hotmail.com>
2020-01-26 21:05:15 +0100

    :pencil2: Implemented user input

--- src/no/hvl/dat109/Stigespill.java
+++ src/no/hvl/dat109/Stigespill.java
@@ -1,5 +1,7 @@
 package no.hvl.dat109;
 
+import java.util.Scanner;
+
 public class Stigespill {
 
 	private final static int WINPOINT = 99;
@@ -7,41 +9,56 @@ public class Stigespill {
 	private Terning terning;
 	private Brett brett;
 	private Spiller[] spillere;
-	private boolean vinner;
+	private Spiller vinner;
 
 	public Stigespill() {
 		this.terning = new Terning();
 		this.brett = new Brett();
-		this.vinner = false;
-		this.spillere = new Spiller[2];
+		this.vinner = null;
 	}
 
 	public void run() {
 
-		spillere[0] = new Spiller("Francis", "grønn");
-		spillere[1] = new Spiller("Lasse", "blå");
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Spillet er klar til å starte.\n Skriv inn antall spillere (2-4)");
+		int antall = sc.nextInt();
+		spillere = new Spiller[antall];
+		
+		for (int i = 0; i < spillere.length; i++) {
+			System.out.println("Skriv inn navn for spiller " + (i+1));
+			String navn = sc.next();
+			System.out.println("Skriv inn farge på brikke for spiller " + (i+1));
+			String farge = sc.next();
+			spillere[i] = new Spiller(navn,farge);
+		}
+		sc.close();
 
-		while (!vinner) {
+		while (vinner == null) {
 			spillRunde();
 		}
-		System.out.println("Noen har vunnet");
+		System.out.println(vinner.getNavn() + " har vunnet");
 	}
 
 	public void spillRunde() {
-
+		
 		for (Spiller s : spillere) {
+			System.out.println(s.getNavn() + " sin tur. Trykk enter for å trille");
+			try {
+				System.in.read();
+			} catch(Exception e) {
+			}
 			s.trill(terning);
-			int verdi = terning.getVerdi();
-			System.out.println(s.getNavn() + " trilte: " + verdi);
 			Rute rute = s.getBrikke().getPlass();
 			System.out.println(s.getNavn() + " var på rute nr: " + rute.getTall());
+			int verdi = terning.getVerdi();
+			System.out.println(s.getNavn() + " trilte: " + verdi);
 			if (rute.getTall() + verdi >= WINPOINT) {
-				vinner = true;
+				vinner = s;
 				break;
 			}
 			Rute nyRute = brett.finnNyRute(rute, verdi);
 			s.getBrikke().setPlass(nyRute);
-			System.out.println(s.getNavn() + " står nå på rute nr: " + s.getBrikke().getPlass().getTall());
+			System.out.println(s.getNavn() + " står nå på rute nr: " + s.getBrikke().getPlass().getTall() + "\n");
 			
 		}
 
